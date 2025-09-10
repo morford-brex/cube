b31477a172eee99e05f266328a63f9d9ffe092ac
Paco Valdez <paco@cube.dev>
2025-09-10 15:21:22 -0700

    feat: Implement drill member inheritance for view cubes and enhance error reporting

--- packages/cubejs-schema-compiler/src/compiler/CubeSymbols.ts
+++ packages/cubejs-schema-compiler/src/compiler/CubeSymbols.ts
@@ -894,8 +894,7 @@ export class CubeSymbols implements TranspilerSymbolResolver {
           name
         );
       // eslint-disable-next-line no-underscore-dangle
-      // if (resolvedSymbol && resolvedSymbol._objectWithResolvedProperties) {
-      if (resolvedSymbol._objectWithResolvedProperties) {
+      if (resolvedSymbol && resolvedSymbol._objectWithResolvedProperties) {
         return resolvedSymbol;
       }
       return cubeEvaluator.pathFromArray(fullPath(cubeEvaluator.joinHints(), [referencedCube, name]));
@@ -1005,7 +1004,7 @@ export class CubeSymbols implements TranspilerSymbolResolver {
           cubeName,
           name
         );
-        if (resolvedSymbol._objectWithResolvedProperties) {
+        if (resolvedSymbol && resolvedSymbol._objectWithResolvedProperties) {
           return resolvedSymbol;
         }
         return '';
